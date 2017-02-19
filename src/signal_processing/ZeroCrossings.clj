89c8aa51cd365ea1fe57b8009fff695fcb2aa194
ssstefan <stefan93ss@hotmail.com>
2017-02-19 02:39:22 +0100

    zero crossings

---
+++ src/signal_processing/ZeroCrossings.clj
@@ -0,0 +1,14 @@
+(ns signal-processing.ZeroCrossings)
+
+(defn extractFeature [samples] (filter (fn [sample] ( sample > 0.0)) samples))
+
+
+
+(defn isCrossPoint [a b] (if (or (and (> a 0) (< b 0)) (and (< a 0) (> b 0)) (and (= a 0) (not= b 0))) true false))
+
+
+(def extractFeature
+  (fn [samples]
+    (loop [i samples count 0]
+      (cond (or (empty? i) (empty? (rest i))) count
+            :t (recur (rest i)(if (isCrossPoint (first i) (second i)) (inc count) count))))))
